bae76575534fc901da0fa0042b8e80951a9483b6
aczender <czender.a@gmail.com>
2017-05-27 21:53:28 +0200

    .

---
+++ app/src/main/java/com/example/android/tenniscountingapp/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.android.tenniscountingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.example.android.tenniscountingapp.R.id.federer_points;
+import static com.example.android.tenniscountingapp.R.id.nadal_points;
+import static com.example.android.tenniscountingapp.R.id.nadal_score;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreNadal = 0;
+    int scoreFederer = 0;
+    int pointsForNadal = 0;
+    int pointsForFederer = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * add 1 score to Nadal
+     */
+    public void plusScoreNadal(View view) {
+        scoreNadal = scoreNadal + 1;
+        displayNadal(scoreNadal);
+    }
+    /**
+     * displays the score for Nadal
+     */
+    public void displayNadal (int score) {
+     TextView scoreView = (TextView) findViewById(nadal_score);
+     scoreView.setText(String.valueOf(score));
+     }
+
+    /**
+     * add 15 points to Nadal
+     */
+    public void addPointsNadal (View view) {
+        pointsForNadal = pointsForNadal + 15;
+        displayForNadal(pointsForNadal);
+        if (pointsForNadal > 45) {
+            scoreNadal = scoreNadal + 1;
+            displayNadal(scoreNadal);
+            Toast.makeText(this, "Point for Nadal", Toast.LENGTH_SHORT).show();
+            pointsForNadal = 0;
+            displayForNadal(pointsForNadal);
+        }
+    }
+     /**
+     * displays the points for Nadal
+     */
+    public void displayForNadal(int score) {
+        TextView scoreView = (TextView) findViewById(nadal_points);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * add 1 score to Federer
+     */
+    public void plusScoreFederer(View view) {
+        scoreFederer = scoreFederer + 1;
+        displayFederer(scoreFederer);
+    }
+
+    /**
+     * displays the score for Federer
+     */
+    public void displayFederer(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.federer_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * add 15 points to Federer
+     */
+    public void addPointsFederer (View view) {
+        pointsForFederer = pointsForFederer + 15;
+        displayForFederer(pointsForFederer);
+        if (pointsForFederer > 45) {
+            scoreFederer = scoreFederer + 1;
+            displayFederer(scoreFederer);
+            Toast.makeText(this, "Point for Federer", Toast.LENGTH_SHORT).show();
+            pointsForFederer = 0;
+            displayForFederer(pointsForFederer);
+        }
+    }
+
+    /**
+     * displays the points for Federer
+     */
+    public void displayForFederer(int score) {
+        TextView scoreView = (TextView) findViewById(federer_points);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Resets the score
+     */
+    public void resetScore (View view){
+       scoreNadal = 0;
+       scoreFederer = 0;
+       displayNadal(scoreNadal);
+       displayFederer(scoreFederer);
+       pointsForNadal = 0;
+       pointsForFederer = 0;
+       displayForNadal(pointsForNadal);
+       displayForFederer(pointsForFederer);
+    }
+}
